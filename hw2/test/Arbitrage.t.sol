dff901033773d534a69abbabb3d8c869ac1fd9d9
4ll3nch3n <97391087+mingeydacoder@users.noreply.github.com>
2024-04-12 21:40:52 +0800

    Update Arbitrage.t.sol

--- hw2/test/Arbitrage.t.sol
+++ hw2/test/Arbitrage.t.sol
@@ -79,6 +79,20 @@ contract Arbitrage is Test {
         /**
          * Please add your solution below
          */
+        address[] memory path = new address[](5);
+        path[0] = address(tokenB);
+        path[1] = address(tokenA);
+        path[2] = address(tokenD);
+        path[3] = address(tokenC);
+        path[4] = address(tokenB);
+
+        uint[] memory amounts = router.swapExactTokensForTokens(
+            5 ether,      
+            1,             
+            path,         
+            arbitrager,    
+            block.timestamp + 1 days  
+        );
         /**
          * Please add your solution above
          */
